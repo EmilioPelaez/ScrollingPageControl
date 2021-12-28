6d583048605914e496ca34df8c0a6963815d5ed4
Emilio Peláez <me@epelaez.net>
2021-12-29 00:03:00 +0100

    Fix Project Warnings

--- Sources/ScrollingPageControl/ScrollingPageControl.swift
+++ Sources/ScrollingPageControl/ScrollingPageControl.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-public protocol ScrollingPageControlDelegate: class {
+public protocol ScrollingPageControlDelegate: AnyObject {
 	//	If delegate is nil or the implementation returns nil for a given dot, the default
 	//	circle will be used. Returned views should react to having their tint color changed
 	func viewForDot(at index: Int) -> UIView?
